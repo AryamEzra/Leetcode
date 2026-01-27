3fed0007704b680074babc5c0e88868e21725c38
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2026-01-27 22:53:50 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (16.59%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.py
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.py
@@ -3,7 +3,10 @@ class Solution:
         l = len(bits)
         i = 0
         while i < l - 1:
-            i += bits[i] + 1
+            if bits[i]  == 1:
+                i += 2
+            else:
+                i += 1
         
         return i == l - 1
         
