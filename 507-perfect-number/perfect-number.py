4528706d877f70b5adeeeb84cbe5aed57b09d035
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2026-02-14 23:02:19 +0300

    Time: 7 ms (44.27%) | Memory: 19.3 MB (66.40%) - LeetSync

---
+++ 507-perfect-number/perfect-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+        div = [1]
+        for i in range(2,int(sqrt(num))+1):
+            if num % i == 0:
+                div.append(i)
+                div.append(num//i)
+        if sum(div) == num:
+            return True
+        return False
+        
