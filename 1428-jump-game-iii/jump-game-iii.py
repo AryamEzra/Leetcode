1d9ca67397fee3b813964305eee63b2c5b135cfa
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2026-05-17 11:25:32 +0300

    Time: 3 ms (99.94%) | Memory: 25.3 MB (83.94%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -2,19 +2,30 @@ class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         # time: O(n)
         # space: O(n)
+
+        #bfs
+
         n = len(arr)
         visited = [False] * n
+        queue = deque([start])
+        visited[start] = True
 
-        def dfs(i):
-            if i < 0 or i >= n or visited[i]:
-                return False
-            
+        while queue:
+            i = queue.popleft()
             if arr[i] == 0:
                 return True
             
-            visited[i] = True
             jump = arr[i]
-            return dfs(i + jump) or dfs(i - jump)
+
+            f = i + jump
+            if 0 <= f < n and not visited[f]:
+                visited[f] = True
+                queue.append(f)
+
+            back = i - jump 
+            if 0 <= back < n and not visited[back]:
+                visited[back] = True
+                queue.append(back)
         
-        return dfs(start)
-        
+
+        return False
