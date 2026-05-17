58930d115575bac21c8171f59d7b86240f4cf046
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2026-05-17 11:35:24 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (16.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,19 +4,14 @@ class Solution:
         # time: O(n)
         # space: O(n)
 
-        cache = {}
+        @cache
 
         def dp(n):
-            if n in cache:
-                return cache[n]
             if n == 1:
                 return 1
             if n == 2:
                 return 2
+            return dp(n-1) + dp(n-2)
 
-            
-            cache[n] = dp(n-1) + dp(n-2)
-            return cache[n]
-        
         return dp(n)
         
