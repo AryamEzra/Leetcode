4c8563301ec6c48b8ba09e99e90d4c77aea3dd3a
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2026-05-17 11:16:07 +0300

    Time: 9 ms (73.38%) | Memory: 39 MB (21.57%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        n = len(arr)
+        visited = [False] * n
+
+        def dfs(i):
+            if i < 0 or i >= n or visited[i]:
+                return False
+            
+            if arr[i] == 0:
+                return True
+            
+            visited[i] = True
+            jump = arr[i]
+            return dfs(i + jump) or dfs(i - jump)
+        
+        return dfs(start)
+        
